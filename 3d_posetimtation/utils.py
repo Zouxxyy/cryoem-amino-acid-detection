07be156333248e097c57faa991fa3d19cd95b44b
roserland <1393745326@qq.com>
2022-01-06 17:06:15 +0800

    for amino 3D pose estimation, begin to constuct a dataset

---
+++ 3d_posetimtation/utils.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@File    :   utils.py    
+@Contact :   roserland@gmail.com
+
+@Modify Time      @Author    @Version    @Desciption
+------------      -------    --------    -----------
+2022/1/5 4:25 下午   fanzw      1.0         None
+'''
+
+# import lib
